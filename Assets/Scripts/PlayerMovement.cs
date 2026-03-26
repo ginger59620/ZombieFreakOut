4684c3bf1548ce24787ca7e5738b8412382865c7
ginger59620 <a59620@alunos.ipb.pt>
2026-03-26 14:15:50 +0000

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetAxisRaw doesn't give acceleration
+        float input = Input.GetAxisRaw("Horizontal");
+        movement.x = input * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
